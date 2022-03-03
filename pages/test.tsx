03e4121fe1b8ac0a3fa4006734da6b1cb6be7371
ansel brandt <ansel@anselbrandt.com>
2022-03-03 13:42:50 -0500

    Add useGetBreakpoints

--- pages/test.tsx
+++ pages/test.tsx
@@ -1,10 +1,13 @@
 import { Box, Link } from "@primer/react";
 import type { NextPage } from "next";
 import ColorModeSwitcher from "../components/ColorModeSwitcher";
+import { useGetBreakpoint } from "../utils/useGetBreakpoint";
 
 interface Props {}
 
 const Test: NextPage<Props> = ({}) => {
+  const breakpoint = useGetBreakpoint();
+  const bg = ["#227C9D", "#17C3B2", "#FFCB77", "#FE6D73"][breakpoint];
   return (
     <Box
       width="100vw"
@@ -14,11 +17,12 @@ const Test: NextPage<Props> = ({}) => {
       alignItems="center"
       justifyContent="center"
       color="fg.default"
-      bg="canvas.default"
+      // bg="canvas.default"
+      bg={bg}
     >
       <ColorModeSwitcher />
       <Link href="/">Home</Link>
-      <Box>Test</Box>
+      <Box>{breakpoint}</Box>
     </Box>
   );
 };
