3fda6691c076d3200e167a3753364b6edea2fac5
ansel brandt <ansel@anselbrandt.com>
2022-03-06 17:26:06 -0500

    Update 404

--- pages/404.tsx
+++ pages/404.tsx
@@ -1,11 +1,24 @@
-import { Box, ThemeProvider, useTheme } from "@primer/react";
+import "@fontsource/libre-baskerville";
+import { HomeIcon } from "@primer/octicons-react";
+import {
+  Box,
+  StyledOcticon,
+  Text,
+  ThemeProvider,
+  useTheme,
+} from "@primer/react";
 import Cookie from "js-cookie";
+import { useRouter } from "next/router";
 import { useEffect, useState } from "react";
+import ColorModeSwitcher from "../components/ColorModeSwitcher";
+import { useGetOrientation } from "../utils/useGetOrientation";
 
 declare type ColorMode = "day" | "night";
 declare type ColorModeWithAuto = ColorMode | "auto";
 
 const Custom404 = () => {
+  const router = useRouter();
+  const { orientation, height } = useGetOrientation();
   const { setDayScheme, setNightScheme, resolvedColorMode } = useTheme();
   const [colorMode, setColorMode] = useState<ColorModeWithAuto>(
     resolvedColorMode || "day"
@@ -37,17 +50,32 @@ const Custom404 = () => {
 
   return (
     <ThemeProvider colorMode={colorMode}>
+      <ColorModeSwitcher />
       <Box
         height="100vh"
         width="100vw"
-        bg="canvas.default"
-        color="fg.default"
         display="flex"
         flexDirection="column"
+        alignItems="center"
         justifyContent="center"
+        bg="canvas.default"
+        color="fg.default"
+        fontFamily="Libre Baskerville"
       >
-        <Box>
-          <img src="/assets/oops.gif" width="100%" height="100%" />
+        <Box fontSize={height && height < 800 ? 50 : [40, 60, 80, 100]}>
+          <Text>Oops.</Text>
+        </Box>
+        <Box
+          mt={75}
+          onClick={() => router.push("/")}
+          sx={{
+            "&:hover": {
+              color: "accent.fg",
+              cursor: "pointer",
+            },
+          }}
+        >
+          <StyledOcticon icon={HomeIcon} size={25} />
         </Box>
       </Box>
     </ThemeProvider>
