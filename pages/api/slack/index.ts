b5f6b9a38a395d56cc6a2a875dd2be36d6fcb44c
ansel brandt <ansel@anselbrandt.com>
2022-03-07 18:52:18 -0500

    Update slack api

--- pages/api/slack/index.ts
+++ pages/api/slack/index.ts
@@ -10,14 +10,19 @@ const handler = async (req: NextApiRequest, res: NextApiResponse) => {
   const body = req.body;
   const challenge = body.challenge;
   res.status(200).send(challenge);
-  const app = new App({
-    token: SLACK_BOT_TOKEN,
-    signingSecret: SLACK_SIGNING_SECRET,
-  });
-  await app.client.chat.postMessage({
-    channel: SLACK_GENERAL_CHANNEL,
-    text: JSON.stringify(body),
-  });
+  try {
+    const app = new App({
+      token: SLACK_BOT_TOKEN,
+      signingSecret: SLACK_SIGNING_SECRET,
+    });
+    const result = await app.client.chat.postMessage({
+      channel: SLACK_GENERAL_CHANNEL,
+      text: JSON.stringify(body),
+    });
+    console.log(result);
+  } catch (error) {
+    console.log(error);
+  }
 };
 
 export default handler;
