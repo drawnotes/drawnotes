0e661e4e6f29610a60b5d4e1a39e64269cc8a641
ansel brandt <ansel@anselbrandt.com>
2022-03-07 16:24:10 -0500

    Update slack endpoint

--- pages/api/slack/index.ts
+++ pages/api/slack/index.ts
@@ -1,8 +1,9 @@
 import { NextApiRequest, NextApiResponse } from "next";
 
 const handler = async (req: NextApiRequest, res: NextApiResponse) => {
-  const query = req.query.challenge;
-  res.status(200).send(query);
+  const body = JSON.parse(req.body);
+  const challenge = body.challenge;
+  res.status(200).send(challenge);
 };
 
 export default handler;
