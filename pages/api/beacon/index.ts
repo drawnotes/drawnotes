e3148ed774b2903421b53da48e079a5610d0ee8a
ansel brandt <ansel@anselbrandt.com>
2022-03-07 00:41:12 -0500

    Cancel navigator beacon in dev

--- pages/api/beacon/index.ts
+++ pages/api/beacon/index.ts
@@ -1,17 +1,19 @@
 import { NextApiRequest, NextApiResponse } from "next";
 import fetch from "node-fetch";
-import { SLACK_WEBHOOK } from "../../../utils/constants";
+import { IS_PROD, SLACK_WEBHOOK } from "../../../utils/constants";
 
 const handler = async (req: NextApiRequest, res: NextApiResponse) => {
   const body = req.body;
-  await fetch(SLACK_WEBHOOK, {
-    method: "POST",
-    headers: {
-      Accept: "application/json",
-      "Content-Type": "application/json",
-    },
-    body: body,
-  });
+  if (IS_PROD) {
+    await fetch(SLACK_WEBHOOK, {
+      method: "POST",
+      headers: {
+        Accept: "application/json",
+        "Content-Type": "application/json",
+      },
+      body: body,
+    });
+  }
   res.status(200).send("OK");
 };
 
