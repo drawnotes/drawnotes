6becde32d402c07bb3fccdd269a11c3585b61fa7
ansel brandt <ansel@anselbrandt.com>
2022-03-08 14:44:57 -0500

    Update response code

--- pages/api/slack/clear/index.ts
+++ pages/api/slack/clear/index.ts
@@ -33,7 +33,7 @@ const handler = async (req: NextApiRequest, res: NextApiResponse) => {
     } catch (error: any) {
       if (error.message !== "An API error occurred: message_not_found") {
         console.log(error);
-        res.status(200).send("something went wrong.");
+        res.status(500).send("something went wrong.");
       }
     }
   } else {
