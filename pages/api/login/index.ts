72f2c6a4e7df4a514ddf88db55394b8db8b1fadb
ansel brandt <ansel@anselbrandt.com>
2022-03-08 14:32:27 -0500

    Fix login route

--- pages/api/login/index.ts
+++ pages/api/login/index.ts
@@ -7,13 +7,17 @@ const getRedirect = async () => {
 
 const handler = async (req: NextApiRequest, res: NextApiResponse) => {
   const referer = req.headers.referer as string;
-  const url = new URL(referer);
-  const path = url.pathname;
-  // const redirect = await getRedirect();
-  const redirect = `https://github.com/login/oauth/authorize?client_id=${GITHUB_CLIENT_ID}&redirect_uri=${
-    GITHUB_CALLBACK + path
-  }&scope=repo%20user%3Aemail`;
-  res.redirect(redirect);
+  if (referer) {
+    const url = new URL(referer);
+    const path = url.pathname;
+    const redirect = `https://github.com/login/oauth/authorize?client_id=${GITHUB_CLIENT_ID}&redirect_uri=${
+      GITHUB_CALLBACK + path
+    }&scope=repo%20user%3Aemail`;
+    res.redirect(redirect);
+  } else {
+    const redirect = await getRedirect();
+    res.redirect(redirect);
+  }
 };
 
 export default handler;
