0d54c111eecd04a3a596d946937ca9ee18579047
ansel brandt <ansel@anselbrandt.com>
2022-03-08 16:46:21 -0500

    Update slack api

--- pages/api/slack/index.ts
+++ pages/api/slack/index.ts
@@ -1,9 +1,24 @@
 import { NextApiRequest, NextApiResponse } from "next";
+const { App } = require("@slack/bolt");
 
 const handler = async (req: NextApiRequest, res: NextApiResponse) => {
   const body = req.body;
   const challenge = body.challenge;
-  res.status(200).send(challenge);
+  if (body.event && body.event.type === "message") {
+    const app = new App({
+      token: process.env.SLACK_BOT_TOKEN,
+      signingSecret: process.env.SLACK_SIGNING_SECRET,
+    });
+    await app.client.chat.postMessage({
+      channel: process.env.SLACK_GENERAL_CHANNEL,
+      text: "Hello, Dave",
+    });
+  }
+  if (challenge) {
+    res.status(200).send(challenge);
+  } else {
+    res.status(200).send("OK");
+  }
 };
 
 export default handler;
