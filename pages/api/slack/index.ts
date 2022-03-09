295775970dbc133a731e8ad1ecc7b1df81fc47e2
ansel brandt <ansel@anselbrandt.com>
2022-03-09 00:53:09 -0500

    Update slack api

--- pages/api/slack/index.ts
+++ pages/api/slack/index.ts
@@ -1,11 +1,21 @@
 import { App } from "@slack/bolt";
 import { NextApiRequest, NextApiResponse } from "next";
-import { SLACK_WEBHOOK } from "../../../utils/constants";
+import {
+  SLACK_BOT_TOKEN,
+  SLACK_SIGNING_SECRET,
+  SLACK_USER_OAUTH_TOKEN,
+  SLACK_WEBHOOK,
+} from "../../../utils/constants";
 
 const handler = async (req: NextApiRequest, res: NextApiResponse) => {
-  const app = new App({
-    token: process.env.SLACK_BOT_TOKEN,
-    signingSecret: process.env.SLACK_SIGNING_SECRET,
+  const botApp = new App({
+    token: SLACK_BOT_TOKEN,
+    signingSecret: SLACK_SIGNING_SECRET,
+  });
+
+  const userApp = new App({
+    token: SLACK_USER_OAUTH_TOKEN,
+    signingSecret: SLACK_SIGNING_SECRET,
   });
 
   const headers = req.headers;
@@ -62,10 +72,28 @@ ${location.ip}`,
   if (event && event.type === "app_mention") {
     const text = event.text;
     const channel = event.channel;
-    await app.client.chat.postMessage({
-      channel: channel,
-      text: "Did you say something?",
-    });
+    if (text.includes("cleanup")) {
+      const results = await botApp.client.search.messages({
+        token: SLACK_USER_OAUTH_TOKEN,
+        query: "drawnotes",
+        sort: "timestamp",
+        sort_dir: "desc",
+      });
+      if (results.messages && results.messages?.matches!.length > 0) {
+        for (const match of results.messages.matches!) {
+          await userApp.client.chat.delete({
+            channel: match.channel!.id as string,
+            ts: match.ts as string,
+            as_user: true,
+          });
+        }
+      }
+    } else {
+      await botApp.client.chat.postMessage({
+        channel: channel,
+        text: "Did you say something?",
+      });
+    }
   }
   res.status(200).send("OK");
 };
