89d33028955e01540da686663365264074091af1
ansel brandt <ansel@anselbrandt.com>
2022-03-09 00:28:15 -0500

    Update slack api

--- pages/api/slack/index.ts
+++ pages/api/slack/index.ts
@@ -1,5 +1,6 @@
+import { App } from "@slack/bolt";
 import { NextApiRequest, NextApiResponse } from "next";
-const { App } = require("@slack/bolt");
+import { SLACK_WEBHOOK } from "../../../utils/constants";
 
 const handler = async (req: NextApiRequest, res: NextApiResponse) => {
   const app = new App({
@@ -10,10 +11,51 @@ const handler = async (req: NextApiRequest, res: NextApiResponse) => {
   const headers = req.headers;
   const body = req.body;
   const event = req.body.event;
-  console.log({ headers });
-  console.log({ body });
-  console.log(JSON.stringify(body.event.blocks, null, 2));
   const challenge = body.challenge;
+  const location = {
+    city: headers["x-vercel-ip-city"],
+    region: headers["x-vercel-ip-country-region"],
+    country: headers["x-vercel-ip-country"],
+    lat: headers["x-vercel-ip-latitude"],
+    long: headers["x-vercel-ip-longitude"],
+    ip: headers["x-real-ip"],
+    ua: headers["user-agent"],
+  };
+  const blocks = {
+    text: `Server request from ${location.city}, ${location.region} ${location.country}`,
+    blocks: [
+      {
+        type: "section",
+        text: {
+          type: "mrkdwn",
+          text: `${location.ua}
+${location.city}, ${location.region} ${location.country}
+${location.ip}`,
+        },
+      },
+      {
+        type: "actions",
+        elements: [
+          {
+            type: "button",
+            text: {
+              type: "plain_text",
+              text: "Google Map",
+            },
+            url: `https://www.google.com/maps/search/?api=1&query=${location.lat},${location.long}`,
+          },
+        ],
+      },
+    ],
+  };
+  await fetch(SLACK_WEBHOOK, {
+    method: "POST",
+    headers: {
+      Accept: "application/json",
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(blocks),
+  });
   if (challenge) {
     res.status(200).send(challenge);
   }
