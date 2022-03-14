52642b3ce5fbad52d7f2759274e0ee3ee60c14ce
ansel brandt <ansel@anselbrandt.com>
2022-03-14 04:20:22 -0400

    Add realtime logging

--- pages/dashboard.tsx
+++ pages/dashboard.tsx
@@ -82,10 +82,16 @@ const Dashboard: NextPage<Props> = ({
       dayScheme={dayScheme}
       nightScheme={nightScheme}
     >
-      <Box color="fg.default" bg="canvas.default" width="100vw" height="100vh">
+      <Box
+        color="fg.default"
+        bg="canvas.default"
+        width="100vw"
+        height="100vh"
+        overflow="hidden"
+      >
         <ColorModeSwitcher />
         <FilterPanel>
-          <LogPanel>
+          <LogPanel data={data}>
             <Box
               height="100%"
               width="100%"
