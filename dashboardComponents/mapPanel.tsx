43cca06ea51d4a9e81bc0ee4f0af6daff709f948
ansel brandt <ansel@anselbrandt.com>
2022-03-14 02:37:09 -0400

    Add map to dashboard

--- dashboardComponents/mapPanel.tsx
+++ dashboardComponents/mapPanel.tsx
@@ -1,10 +1,65 @@
-import { Box } from "@primer/react";
+import DeckGL from "@deck.gl/react";
+import { Box, useTheme } from "@primer/react";
+import "mapbox-gl/dist/mapbox-gl.css";
 import { NextPage } from "next";
+import { useEffect, useState } from "react";
+import { StaticMap, WebMercatorViewport } from "react-map-gl";
+import { MAPBOX_ACCESS_TOKEN } from "../utils/constants";
 
-interface Props {}
+const DARK_MAP_STYLE =
+  "https://basemaps.cartocdn.com/gl/dark-matter-gl-style/style.json";
 
-const mapPanel: NextPage<Props> = ({}) => {
-  return <Box>Map</Box>;
+const LIGHT_MAP_STYLE =
+  "https://basemaps.cartocdn.com/gl/positron-gl-style/style.json";
+
+interface Props {
+  mapSize: { width: number; height: number };
+}
+
+const mapPanel: NextPage<Props> = ({ mapSize }) => {
+  const { colorScheme } = useTheme();
+  const mapStyle = colorScheme!.includes("dark")
+    ? DARK_MAP_STYLE
+    : LIGHT_MAP_STYLE;
+  const [viewState, setViewState] = useState<any>(null);
+  const bounding = [-73.956862, 45.402657, -73.480099, 45.701392];
+
+  useEffect(() => {
+    if (typeof window !== "undefined") {
+      const viewport = new WebMercatorViewport({
+        width: window.innerWidth,
+        height: window.innerHeight,
+      }).fitBounds([bounding.slice(0, 2), bounding.slice(2, 4)] as [
+        [number, number],
+        [number, number]
+      ]);
+      setViewState(viewport);
+    }
+  }, [bounding]);
+
+  const handleChangeViewState = ({ viewState }: any) => {
+    setViewState(viewState);
+  };
+
+  return (
+    <Box position="relative" height="100%">
+      <DeckGL
+        width={mapSize.width}
+        height={mapSize.height}
+        layers={[]}
+        viewState={viewState}
+        onViewStateChange={handleChangeViewState}
+        controller={true}
+      >
+        <StaticMap
+          reuseMaps
+          mapStyle={mapStyle}
+          mapboxApiAccessToken={MAPBOX_ACCESS_TOKEN}
+          preventStyleDiffing={true}
+        />
+      </DeckGL>
+    </Box>
+  );
 };
 
 export default mapPanel;
--- pages/dashboard.tsx
+++ pages/dashboard.tsx
@@ -1,22 +1,85 @@
-import { Box } from "@primer/react";
+import { Box, ThemeProvider } from "@primer/react";
 import { NextPage } from "next";
+import { useEffect, useRef, useState } from "react";
 import ColorModeSwitcher from "../components/ColorModeSwitcher";
 import FilterPanel from "../dashboardComponents/filterPanel";
 import LogPanel from "../dashboardComponents/logPanel";
 import MapPanel from "../dashboardComponents/mapPanel";
 
-interface Props {}
+declare type ColorMode = "day" | "night";
+declare type ColorModeWithAuto = ColorMode | "auto";
 
-const Dashboard: NextPage<Props> = ({}) => {
+interface Props {
+  preferredColorMode: ColorModeWithAuto;
+  preferredDayScheme: string;
+  preferredNightScheme: string;
+}
+
+const Dashboard: NextPage<Props> = ({
+  preferredColorMode,
+  preferredDayScheme,
+  preferredNightScheme,
+}) => {
+  const [colorMode, setColorMode] = useState<ColorModeWithAuto>(
+    preferredColorMode || "day"
+  );
+  const [dayScheme, setDayScheme] = useState(preferredDayScheme || "light");
+  const [nightScheme, setNightScheme] = useState(
+    preferredNightScheme || "dark"
+  );
+  const deckRef = useRef<HTMLDivElement>(null);
+  const observer = useRef<ResizeObserver>();
+  const [mapSize, setMapSize] = useState<any>();
+
+  useEffect(() => {
+    function handleResize() {
+      if (deckRef.current) {
+        const width = deckRef.current.clientWidth;
+        const height = deckRef.current.clientHeight;
+        setMapSize({ width: width, height: height });
+      }
+    }
+    window.addEventListener("resize", handleResize);
+    handleResize();
+    return () => window.removeEventListener("resize", handleResize);
+  }, []);
+
+  useEffect(() => {
+    if (typeof window !== undefined) {
+      observer.current = new ResizeObserver((entries) => {
+        const { width, height } = entries[0].contentRect;
+        setMapSize({ width: width, height: height });
+      });
+    }
+  }, []);
+
+  useEffect(() => {
+    if (observer.current && deckRef.current) {
+      observer.current.observe(deckRef.current);
+    }
+    return () => {
+      if (observer.current && deckRef.current !== null) {
+        observer.current.unobserve(deckRef.current);
+      }
+    };
+  }, [deckRef, observer]);
   return (
-    <Box color="fg.default" bg="canvas.default" width="100vw" height="100vh">
-      <ColorModeSwitcher />
-      <FilterPanel>
-        <LogPanel>
-          <MapPanel />
-        </LogPanel>
-      </FilterPanel>
-    </Box>
+    <ThemeProvider
+      colorMode={colorMode}
+      dayScheme={dayScheme}
+      nightScheme={nightScheme}
+    >
+      <Box color="fg.default" bg="canvas.default" width="100vw" height="100vh">
+        <ColorModeSwitcher />
+        <FilterPanel>
+          <LogPanel>
+            <Box height="100%" width="100%" ref={deckRef}>
+              {mapSize && <MapPanel mapSize={mapSize} />}
+            </Box>
+          </LogPanel>
+        </FilterPanel>
+      </Box>
+    </ThemeProvider>
   );
 };
 
