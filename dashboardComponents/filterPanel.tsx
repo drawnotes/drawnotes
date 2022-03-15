88bff78b93c1e6f3a2af7477f5e9953e1cc6b688
ansel brandt <ansel@anselbrandt.com>
2022-03-15 02:22:24 -0400

    Style filterPanel

--- dashboardComponents/filterPanel.tsx
+++ dashboardComponents/filterPanel.tsx
@@ -1,4 +1,4 @@
-import { Box, Text } from "@primer/react";
+import { Box, Checkbox, FormControl, Text } from "@primer/react";
 import { NextPage } from "next";
 import Image from "next/image";
 import { useRouter } from "next/router";
@@ -44,19 +44,63 @@ const filterPanel: NextPage<Props> = ({ children }) => {
           </Box>
           <Box m={4}>
             <Box mb={2}>Transit</Box>
-            <Box>transit filters</Box>
+            <Box>
+              <form>
+                <FormControl>
+                  <Checkbox id="vehicles" />
+                  <FormControl.Label>Vehicles</FormControl.Label>
+                </FormControl>
+                <FormControl>
+                  <Checkbox id="paths" />
+                  <FormControl.Label>Paths</FormControl.Label>
+                </FormControl>
+                <FormControl>
+                  <Checkbox id="routes" />
+                  <FormControl.Label>Routes</FormControl.Label>
+                </FormControl>
+                <FormControl>
+                  <Checkbox id="stops" />
+                  <FormControl.Label>Stops</FormControl.Label>
+                </FormControl>
+              </form>
+            </Box>
           </Box>
           <Box m={4}>
             <Box mb={2}>Bike Paths</Box>
-            <Box>bike path filters</Box>
+            <Box>
+              <form>
+                <FormControl>
+                  <Checkbox id="separated" />
+                  <FormControl.Label>Separated</FormControl.Label>
+                </FormControl>
+                <FormControl>
+                  <Checkbox id="shared" />
+                  <FormControl.Label>Shared</FormControl.Label>
+                </FormControl>
+                <FormControl>
+                  <Checkbox id="multiUse" />
+                  <FormControl.Label>Multi-Use</FormControl.Label>
+                </FormControl>
+              </form>
+            </Box>
           </Box>
-          <Box m={4} overflow="scroll" height="100%">
+          <Box
+            m={4}
+            p={2}
+            overflow="scroll"
+            height="60vh"
+            borderColor="border.default"
+            borderWidth={1}
+            borderStyle="solid"
+            borderRadius={2}
+            bg="canvas.default"
+          >
             {routeNames.data.map((route, index) => {
               if (index === routeNames.data.length - 1) {
                 return (
                   <Box
                     key={index}
-                    mb="400px"
+                    mb="200px"
                   >{`${route.route_id} ${route.route_long_name},`}</Box>
                 );
               } else {
